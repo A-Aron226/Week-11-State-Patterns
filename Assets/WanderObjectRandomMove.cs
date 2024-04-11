525f5a376ef520b54aa9da8fb11d4e46ce7bc3b9
A-Aron226 <aroeder345@gmail.com>
2024-04-11 13:14:26 -0500

    testing wander in wander state

--- Assets/WanderObjectRandomMove.cs
+++ Assets/WanderObjectRandomMove.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WanderObjectRandomMove : State
 {
+    NavMeshAgent agent;
+    float wanderLocation = 10.0f;
+    Vector3 startingLocation;
     public WanderObjectRandomMove(StateMachine m) : base(m)
     {
 
     }
     public override void EnterState() //overriding from State class
     {
-        Debug.Log("moving");
+        //agent = GetComponent<NavMeshAgent>();
+        //startingLocation = transform.position;
     }
     public override void UpdateState()
     {
         Debug.Log("moving");
+        GetRandomPoint();
     }
 
     public override void ExitState()
     {
         
+    }
+
+    public void GoToRandomPosition()
+    {
+        agent.SetDestination(GetRandomPoint());
+    }
+    Vector3 GetRandomPoint()
+    {
+        Vector3 offset = new Vector3(Random.Range(-wanderLocation, wanderLocation), 0, Random.Range(-wanderLocation, wanderLocation)); //Random position the enemy object can move toward.
+
+        NavMeshHit hit;
+
+        bool gotPoint = NavMesh.SamplePosition(startingLocation + offset, out hit, 1, NavMesh.AllAreas);
+
+        if (gotPoint)
+        {
+            return hit.position;
+        }
+
+        return Vector3.zero;
     }
 }
